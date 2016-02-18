3b242713442da8dbbbde3fb5b1f0adb70d3d65bf
Andreas Göransson <ag@santiclaws.se>
2016-02-18 22:39:48 +0100

    Updated the test on the secret

--- index.js
+++ index.js
@@ -15,40 +15,25 @@ module.exports = {
     if (parts.length !== 3) {
       return false;
     }
-    
+
     var correctCharacters = new RegExp('[A-Za-z0-9+/=]');
 
     // Check if the header and data is Base64
     if (!correctCharacters.test(decodeURIComponent(parts[0])) || !correctCharacters.test(decodeURIComponent(parts[1]))) {
       return false;
     }
-    
+
     // Get the three separate parts, return false if the decode fails
-    var header = undefined;
-    try {
-      header = JSON.parse(window.atob(decodeURIComponent(parts[0])));
-    } catch (err) {
-      return false;
-    }
-    var data = undefined;
-    try {
-      data = JSON.parse(window.atob(decodeURIComponent(parts[1])));
-    } catch (err) {
-      return false;
-    }
-    var secret = undefined;
-    try {
-      secret = window.atob(decodeURIComponent(parts[2]));
-    } catch (err) {
-      return false;
-    }
+    var header = new Buffer(unescape(parts[0]), 'base64').toString('ascii');
+    var data = new Buffer(unescape(parts[1]), 'base64').toString('ascii');
+    var secret = new Buffer(unescape(parts[2]), 'base64').toString('ascii');
 
     // Test the secret, it should be a UUID
-    var validSecret = new RegExp('/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i');
+    var validSecret = new RegExp('[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$');
     if (!validSecret.test(secret)) {
       return false;
     }
-    
+
     // Return them as an array
     return [header, data, secret];
   }
--- test/index.js
+++ test/index.js
@@ -15,5 +15,13 @@ describe('#decode', function() {
   it('should return false for bad token (4)', function () {
     decode('last.bad.token').should.equal(false);
   });
-  // TODO: More tests...
+  
+  // Acceptable token
+  var token = 'eyJhbGciOiJBRVMtMjU2LUNCQyIsInR5cCI6ImxpdHRlcmJveCJ9.eyJ1dWlkIjoiNmUxZTc2ZWMtZDc2My00ZDAxLWE2NDMtMzM0OTdiOTdmZGIwIiwibmFtZSI6IkFuZHJlYXMgR1x1MDBmNnJhbnNzb24iLCJyb2xlIjoic3VwZXJhZG1pbiJ9.YzU3Y2NlMTItZDY4My0xMWU1LTk4ZGUtMDQwMTgxYTVjZTAx';
+  it('should be an array', function () {
+    decode(token).to.be.instanceof(Array);
+  });
+  it('should have a length of three', function () {
+    decode(token).length.to.equal(2);
+  });
 });
